b2db72b677bcb32135552661a5bcac8c3986df7a
5ergiu <sergiu.a.bugeac@gmail.com>
2021-01-20 19:57:57 +0200

    ascsca

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,68 @@
-FROM php:8.0-fpm
+FROM php:8.0-fpm-alpine
 
-# Arguments defined in docker-compose.yml
-ARG user
-ARG uid
+ARG PHP_EXTENSIONS="dom mbstring curl gd intl pdo mysqli pdo_mysql session zip"
 
-# Install system dependencies
-RUN apt-get update && apt-get install -y \
-    git \
-    curl \
-    libpng-dev \
-    libonig-dev \
-    libxml2-dev \
-    zip \
-    unzip
+# Install dependencies
+RUN apk add --update \
+		autoconf \
+		curl \
+		ca-certificates \
+		freetype-dev \
+		gcc \
+		icu-dev \
+		libcurl \
+        curl-dev \
+		libffi-dev \
+		libgcrypt-dev \
+		libjpeg-turbo-dev \
+		libmcrypt-dev \
+		libpng-dev \
+		libpq \
+		libressl-dev \
+		libxslt-dev \
+		libzip-dev \
+		linux-headers \
+		make \
+		musl-dev \
+		nginx \
+        oniguruma-dev \
+		supervisor && \
+		pecl install -f xdebug && \
+    docker-php-ext-enable xdebug && \    
+	docker-php-ext-install $PHP_EXTENSIONS && \
+    echo -e "xdebug.mode=develop\n\
+    xdebug.var_display_max_children = -1\n\
+    xdebug.var_display_max_depth = -1\n\
+    xdebug.var_display_max_data = -1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini && \ 
+	docker-php-source delete && \
+	apk del --purge gcc musl-dev linux-headers libffi-dev make autoconf && \
+	rm -rf /var/cache/apk/* && \
+	rm /etc/nginx/conf.d/default.conf && \
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp && \
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/usr/local/bin --filename=composer && \
+	php -r "unlink('composer-setup.php');"
 
-# Install PHP extensions
-RUN docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd
+# Symling php8 => php
+RUN ln -s /usr/bin/php8 /usr/bin/php
 
-# Get latest Composer
-COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+# Configure nginx
+COPY .docker/nginx.conf /etc/nginx/nginx.conf
 
-# Create system user to run Composer and Artisan Commands
-RUN useradd -G www-data,root -u $uid -d /home/$user $user
-RUN mkdir -p /home/$user/.composer && \
-    chown -R $user:$user /home/$user
+# Configure PHP-FPM
+COPY .docker/fpm-pool.conf /etc/php8/php-fpm.d/www.conf
 
-# Set working directory
-WORKDIR /var/www/app
+# Configure supervisord
+COPY .docker/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 
-USER $user
+# Setup document root
+RUN mkdir -p /var/www/blog
+
+# Add application
+WORKDIR /var/www/blog
+COPY . /var/www/blog/
+
+# Expose the port nginx is reachable on
+EXPOSE 80
+
+# Let supervisord start nginx & php-fpm
+CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor/conf.d/supervisord.conf"]
---
+++ src/Core/Model/QueryBuilder.php
@@ -0,0 +1,425 @@
+<?php
+namespace App\Core\Model;
+
+use App\Core\Exception\HaveToWorkOnTheseExceptions;
+use App\Entity\AbstractEntity as Entity;
+use Exception;
+use PDO;
+use PDOException;
+use PDOStatement;
+
+/**
+ * Builds queries.
+ * @property string $table                The name of the table.
+ * @property PDO $pdo                     PDO instance.
+ * @property array $attributes            The table's attributes.
+ * @property string $alias                The table alias.
+ * @property string|null $selections      The selected fields('*' by default).
+ * @property string|null $joins           Query joins.
+ * @property array|null $parameters       Query parameters.
+ * @property string|null $conditions      Query conditions.
+ * @property string|null $groupBy         Query group by conditions.
+ * @property string|null $orderBy         Query order by conditions.
+ * @property string|null $limit           Query limit.
+ * @property string $query                Query to be executed on the database.
+ * @property PDOStatement|null $statement PDO Statement to be executed.
+ */
+class QueryBuilder extends Query
+{
+    private string $table;
+    private PDO $pdo;
+    private array $attributes;
+    private string $alias;
+    private ?string $selections = null;
+    private ?string $joins = null;
+    private ?array $parameters = null;
+    private ?string $conditions = null;
+    private ?string $groupBy = null;
+    private ?string $orderBy = null;
+    private ?string $limit = null;
+    private string $query;
+    public PDOStatement $statement;
+
+    /**
+     * QueryBuilder constructor.
+     * @param string $table
+     * @throws Exception
+     */
+    public function __construct(string $table)
+    {
+        $this->table = $table;
+        $this->pdo = $this->__connect();
+        $this->__setAttributesFromDatabaseSchema();
+    }
+
+    /**
+     * Connects to the database and returns the PDO object.
+     * @return PDO
+     * @throws Exception
+     */
+    private static function __connect(): PDO
+    {
+        $dsn = "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']}";
+        // var_dump($dsn); die;
+        try {
+            $pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS']);
+            // set the PDO error mode to exception
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // set default fetch mode to fetch associative
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $pdo;
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * Queries the database for the table's attributes and sets them.
+     * @return void
+     * @throws Exception
+     */
+    private function __setAttributesFromDatabaseSchema(): void
+    {
+        $sql = "
+            SELECT COLUMN_NAME
+            FROM INFORMATION_SCHEMA.COLUMNS
+            WHERE TABLE_SCHEMA = :db_name AND
+            TABLE_NAME = :table_name AND
+            COLUMN_KEY <> 'PRI'
+        ";
+        try {
+            $query = $this->pdo->prepare($sql);
+            $query->execute([
+                ':db_name' => $_ENV['DB_NAME'],
+                ':table_name' => $this->table,
+            ]);
+            $columns = $query->fetchAll();
+            foreach ($columns as $column) {
+                foreach ($column as $key => $value) {
+                    $this->attributes[] = $value;
+                }
+            }
+        } catch (PDOException $e) {
+            throw new HaveToWorkOnTheseExceptions($e);
+        }
+    }
+
+    /**
+     * @param string $alias
+     * @return $this
+     */
+    public function setAlias(string $alias): static
+    {
+        $this->alias = $alias;
+        return $this;
+    }
+
+    /**
+     * @param array $selections
+     * @return $this
+     */
+    public function select(array $selections): static
+    {
+        $tempSelections = [];
+        foreach ($selections as $selection => $alias) {
+            if (is_int($selection)) {
+                $tempSelections[] = "$alias";
+            } else {
+                $tempSelections[] = "$selection as $alias";
+            }
+        }
+        $this->selections = implode(', ', $tempSelections);
+        $this->selections = "{$this->selections} FROM {$this->table} as {$this->alias} ";
+        return $this;
+    }
+
+    /**
+     * @param array $joins
+     * @return $this
+     */
+    public function joins(array $joins): static
+    {
+        foreach ($joins as $join) {
+            $this->joins .= "{$join['type']} JOIN {$join['table']} as {$join['alias']} ON ";
+            foreach ($join['conditions'] as $type => $condition) {
+                if (!is_array($condition)) {
+                    if ($type !== array_key_last($join['conditions'])) {
+                        $this->joins .= "$condition AND";
+                    } else {
+                        $this->joins .= "$condition ";
+                    }
+                } else {
+                    foreach ($condition as $key => $value) {
+                        if ($key !== array_key_last($condition)) {
+                            $this->joins .= "$value $type";
+                        } else {
+                            $this->conditions .= "$value ";
+                        }
+                    }
+                }
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @param array $conditions
+     * @return $this
+     */
+    public function where(array $conditions): static
+    {
+        $this->conditions .= 'WHERE ';
+        foreach ($conditions as $type => $condition) {
+            if (!is_array($condition)) {
+                if ($type !== array_key_last($conditions)) {
+                    $this->conditions .= "$condition $type ";
+                } else {
+                    $this->conditions .= "$condition ";
+                }
+            } else {
+                foreach ($condition as $key => $value) {
+                    if ($key !== array_key_last($condition)) {
+                        $this->conditions .= "$value $type ";
+                    } else {
+                        $this->conditions .= "$value ";
+                    }
+                }
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @param array $parameters
+     * @return $this
+     */
+    public function setParameters(array $parameters): static
+    {
+        $this->parameters = $parameters;
+        return $this;
+    }
+
+    /**
+     * @param array $group
+     * @return $this
+     */
+    public function groupBy(array $group): static
+    {
+        $this->groupBy .= 'GROUP BY ';
+        foreach ($group as $key => $groupBy) {
+            if ($key !== array_key_last($group)) {
+                $this->groupBy .= "$groupBy, ";
+            } else {
+                $this->groupBy .= "$groupBy ";
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @param array $orderBy
+     * @return $this
+     */
+    public function orderBy(array $orderBy): static
+    {
+        $this->orderBy .= 'ORDER BY ';
+        foreach ($orderBy as $sort => $order) {
+            if (is_int($sort)) {
+                if ($sort !== array_key_last($orderBy)) {
+                    $this->orderBy .= "$order, ";
+                } else {
+                    $this->orderBy .= "$order ";
+                }
+            } else {
+                if ($sort !== array_key_last($orderBy)) {
+                    $this->orderBy .= "$sort $order, ";
+                } else {
+                    $this->orderBy .= "$sort $order ";
+                }
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @param int|null $limit
+     * @return $this
+     */
+    public function setMaxResults(?int $limit): static
+    {
+        if ($limit !== null) {
+            $this->limit .= "LIMIT $limit";
+        }
+        return $this;
+    }
+
+    /**
+     * Builds the final query string to ensure the correct order is followed.
+     * @return $this
+     */
+    public function getQuery(): static
+    {
+        $this->query = 'SELECT ';
+        if (!empty($this->selections)) {
+            $this->query .= $this->selections;
+        } else {
+            $this->query .= "* FROM $this->table as $this->alias ";
+        }
+        if (!empty($this->joins)) {
+            $this->query .= $this->joins;
+        }
+        if (!empty($this->conditions)) {
+            $this->query .= $this->conditions;
+        }
+        if (!empty($this->groupBy)) {
+            $this->query .= $this->groupBy;
+        }
+        if (!empty($this->orderBy)) {
+            $this->query .= $this->orderBy;
+        }
+        if (!empty($this->limit)) {
+            $this->query .= $this->limit;
+        }
+        $this->query = rtrim($this->query);
+        $this->query .= ';';
+        $this->__reset();
+        return $this;
+    }
+
+    /**
+     * Reset everything in case we create another query from another method right after using the previous query.
+     * @return void
+     */
+    private function __reset(): void
+    {
+        $this->selections = null;
+        $this->joins = null;
+        $this->conditions = null;
+        $this->groupBy = null;
+        $this->orderBy = null;
+        $this->limit = null;
+    }
+
+    /**
+     * Returns the formatted query string.
+     * @return string
+     */
+    public function toQueryString(): string
+    {
+        return $this->query;
+    }
+
+    /**
+     * Prepares the query and binds the values.
+     * @return void
+     * @throws HaveToWorkOnTheseExceptions
+     */
+    private function __prepareStatement(): void
+    {
+        $statement = $this->pdo->prepare($this->query);
+        if (!empty($this->parameters)) {
+            foreach ($this->parameters as $parameter => $value) {
+                $statement->bindValue(":$parameter", $value);
+            }
+        }
+        if ($statement !== false && $statement instanceof PDOStatement) {
+            $this->statement = $statement;
+        } else {
+            throw new HaveToWorkOnTheseExceptions;
+        }
+        $this->__reset();
+    }
+
+    /**
+     * Returns an array with the values stored in an entity.
+     * @param Entity $entity
+     * @return array
+     */
+    private function __getValuesFromEntity(Entity $entity): array
+    {
+        $values = [];
+        foreach ($this->attributes as $attribute) {
+            $attribute = str_replace('_', '', lcfirst(ucwords($attribute, '_')));
+            // Default values set in entities are easy to access because of this.
+            $funcName = 'get' . ucwords($attribute);
+            if (property_exists($entity, $attribute)) {
+                if (method_exists($entity, $funcName)) {
+                    $values[$attribute] = $entity->{$funcName}();
+                }
+            }
+        }
+        return $values;
+    }
+
+    /**
+     * Saves a new record to the database.
+     * @param Entity $entity
+     * @return int|false
+     * @throws Exception
+     */
+    public function getLastInsertedId(Entity $entity): int|false
+    {
+        $parameters = $this->__getValuesFromEntity($entity);
+        foreach ($parameters as $attribute => $value) {
+            $formattedAttribute = strtolower(preg_replace('/(?<!^)[A-Z]/','_$0', $attribute));
+            $this->parameters[$formattedAttribute] = $value;
+        }
+        $attributes = implode(', ', array_keys($this->parameters));
+        $prepareAttributes = implode(', ', array_map(fn($attr) => ":$attr", array_keys($this->parameters)));
+        $this->query = "INSERT INTO $this->table($attributes) VALUES ($prepareAttributes)";
+        $this->__prepareStatement();
+        if ($this->insert($this->statement)) {
+            return $this->pdo->lastInsertId();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns one or more rows from the table.
+     * @return array|null
+     * @throws Exception
+     */
+    public function getResults(): ?array
+    {
+        $this->__prepareStatement();
+        return $this->fetchAll($this->statement);
+    }
+
+    /**
+     * Returns a row from the table.
+     * @return mixed
+     * @throws Exception
+     */
+    public function firstOrNull(): mixed
+    {
+        $this->__prepareStatement();
+        return $this->fetch($this->statement);
+    }
+
+    /**
+     * Returns the count of a specific query.
+     * @return mixed
+     * @throws Exception
+     */
+    public function count(): mixed
+    {
+        $this->__prepareStatement();
+        return $this->fetchColumn($this->statement);
+    }
+
+    /**
+     * Deletes a record from the database.
+     * @param int $id The id based on which the record will be deleted.
+     * @return bool
+     * @throws Exception
+     */
+    public function remove(int $id): bool
+    {
+        $this->setParameters(['id' => $id]);
+        $prepareAttributes = "{$this->table}.id = :id";
+        $this->query = "DELETE FROM {$this->table} WHERE $prepareAttributes LIMIT 1";
+        $this->__prepareStatement();
+        return $this->delete($this->statement);
+    }
+}
