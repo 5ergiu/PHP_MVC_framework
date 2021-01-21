405b69cb89a8b5aabd4de28c1cbc6872c69aaa55
5ergiu <sergiu.a.bugeac@gmail.com>
2021-01-21 16:57:17 +0200

    apsocmp

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 // The server/domain name
-define('HOST', 'http://blog.local');
+define('HOST', 'http://blog.local:8082');
 // Assets paths used in frontend
 define('ASSETS_CSS', HOST . '/css/');
 define('ASSETS_JS', HOST . '/js/');
